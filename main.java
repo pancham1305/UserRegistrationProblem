02f96308bdb428fda69023570324c32d21e88723
Pancham Mehta <pancham1305@gmail.com>
2024-12-13 11:00:32 +0530

    created regex for last name

--- main.java
+++ main.java
@@ -5,10 +5,13 @@ public class main {
     public static void main(String[] args) {
         String regUser = "^[A-Z][a-zA-Z]{2,}$";
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        if (!name.matches(regUser)) {
-            System.out.println("Invalid Name");
+        String firstName = sc.next();
+        if (!firstName.matches(regUser)) {
+            System.out.println("Invalid First Name");
         }
-        
+        String lastName = sc.next();
+        if (!lastName.matches(regUser)) {
+            System.out.println("Invalid Last Name");
+        }
     }
 }
