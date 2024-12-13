a297012b4c0ac571538106969810ae67097464f9
Pancham Mehta <pancham1305@gmail.com>
2024-12-13 12:02:45 +0530

    password Rule 2

--- main.java
+++ main.java
@@ -23,11 +23,11 @@ public class main {
         if (!number.matches(numReg)) {
             System.out.println("Invalid Number");
         }
-        String passwordReg = "^(?=.*[A-Z]).{8,}$";
+        String passwordReg = "^(?=.*[A-Z])(?=.*\\d).{8,}$";
         String password = sc.next();
         if (!password.matches(passwordReg)) {
             System.out.println("Invalid Password");
         }
-        
+
     }
 }
