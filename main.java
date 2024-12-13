f8a2df1af76e5304cf965fd1a85503d2ebeb00fd
Pancham Mehta <pancham1305@gmail.com>
2024-12-13 10:49:40 +0530

    created regex for first name

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        String regUser = "^[A-Z][a-zA-Z]{2,}$";
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        if (!name.matches(regUser)) {
+            System.out.println("Invalid Name");
+        }
+        
+    }
+}
